747d08ad22ec13a3f6ac5e3835898f709b348371
Alex <steblyakov.aleksandr@gmail.com>
2026-07-09 23:55:50 +0300

    continuing implementing config setup

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,9 @@ import { ConfigModule } from '@nestjs/config';
 
 export const configModule = ConfigModule.forRoot({
     envFilePath: [
-        process.env.ENV_FILE_PATH?.trim(),
-        '.env.development',
+        process.env.ENV_FILE_PATH?.trim() ?? '.env.development',
+        `.env.${process.env.NODE_ENV}.local`,
+        `.env.${process.env.NODE_ENV}`,
         '.env.production',
     ],
 });
