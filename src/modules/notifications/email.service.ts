6e758b6650ba89d25973dc7ded76298b6615c1f9
Alex <steblyakov.aleksandr@gmail.com>
2026-08-13 16:18:21 +0300

    Lesson 16 - platform testing, fixing long resolving times for email-sender by excluding await

--- src/modules/notifications/email.service.ts
+++ src/modules/notifications/email.service.ts
@@ -1,5 +1,5 @@
-import {Injectable} from "@nestjs/common";
-import {MailerService} from "@nestjs-modules/mailer";
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class EmailService {
@@ -7,15 +7,24 @@ export class EmailService {
 
     async sendConfirmationEmail(email: string, code: string): Promise<void> {
         //can add html templates, implement advertising and other logic for mailing...
-        await this.mailerService.sendMail({
-            subject: `finish registration`,
-            to: email,
-            text: `finish registration via link https://somesite.com/confirm-registration?code=${code}`,
-            html: `<h1>Registration completion</h1>
+        this.mailerService
+            .sendMail({
+                subject: `finish registration`,
+                to: email,
+                text: `finish registration via link https://somesite.com/confirm-registration?code=${code}`,
+                html: `<h1>Registration completion</h1>
         <p>To finish registration please follow the link below:
             <a href="https://somesite.com/confirm-registration?code=${code}">complete registration</a>
         </p>`,
-        });
+            })
+            .catch((err) => {
+                console.error(
+                    'Password recovery email failed in background:',
+                    err,
+                );
+            });
+
+        return Promise.resolve();
     }
 
     // async sendConfirmationEmail(email: string, code: string): Promise<void> {
@@ -32,14 +41,23 @@ export class EmailService {
     // }
 
     async sendRecoveryEmail(email: string, code: string): Promise<void> {
-        await this.mailerService.sendMail({
-            subject: `password recovery`,
-            to: email,
-            text: `confirm password recovery via link https://somesite.com/password-recovery?code=${code}`,
-            html: `<h1>Password recovery</h1>
+        this.mailerService
+            .sendMail({
+                subject: `password recovery`,
+                to: email,
+                text: `confirm password recovery via link https://somesite.com/password-recovery?code=${code}`,
+                html: `<h1>Password recovery</h1>
         <p>To confirm password recovery please follow the link below:
             <a href="https://somesite.com/password-recovery?code=${code}">recovery password</a>
-        </p>`
-        });
+        </p>`,
+            })
+            .catch((err) => {
+                console.error(
+                    'Password recovery email failed in background:',
+                    err,
+                );
+            });
+
+        return Promise.resolve();
     }
 }
